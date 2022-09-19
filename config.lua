ef59cf764215fff95e47ad454ec3a164465a26f6
MaDHouSe79 <madhouse1979@gmail.com>
2022-09-19 16:40:52 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,4 +3,6 @@ Config.KeyScriptTrigger = "qb-vehiclekeys:server:AcquireVehicleKeys"
 Config.ParkingCommand   = "park-menu"
 Config.ParkingKeybinds  = "F6"
 
+Config.AdminOnly        = true
+
 Config.AutoStartVehicle = true
