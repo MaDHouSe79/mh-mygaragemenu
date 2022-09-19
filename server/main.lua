970eb14d36d221df74cafe32f1805f0d5f21f3ce
MaDHouSe79 <madhouse1979@gmail.com>
2022-09-19 22:40:04 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -13,21 +13,21 @@ QBCore.Functions.CreateCallback('mh-mygaragemenu:server:isAdmin', function(sourc
     cb(isAllowed)
 end)
 
-RegisterNetEvent('qb-mygaragemenu:server:GetVehicleOutGarage', function(plate)
+RegisterNetEvent('mh-mygaragemenu:server:GetVehicleOutGarage', function(plate)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local citizenid = Player.PlayerData.citizenid
     MySQL.Async.execute('UPDATE player_vehicles SET state = ? WHERE plate = ? AND citizenid = ?', {0, plate, citizenid})		
 end)
 
-RegisterNetEvent('qb-mygaragemenu:server:PutVehicleInGarage', function(plate)
+RegisterNetEvent('mh-mygaragemenu:server:PutVehicleInGarage', function(plate)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local citizenid = Player.PlayerData.citizenid
     MySQL.Async.execute('UPDATE player_vehicles SET state = ? WHERE plate = ? AND citizenid = ?', {1, plate, citizenid})		
 end)
 
-QBCore.Functions.CreateCallback('qb-mygaragemenu:server:isOwner', function(source, cb, plate)
+QBCore.Functions.CreateCallback('mh-mygaragemenu:server:isOwner', function(source, cb, plate)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local citizenid = Player.PlayerData.citizenid
@@ -40,7 +40,7 @@ QBCore.Functions.CreateCallback('qb-mygaragemenu:server:isOwner', function(sourc
 	end)
 end)
 
-QBCore.Functions.CreateCallback("qb-mygaragemenu:server:GetVehicleProperties", function(source, cb, plate)
+QBCore.Functions.CreateCallback("mh-mygaragemenu:server:GetVehicleProperties", function(source, cb, plate)
     local properties = {}
     local result = MySQL.query.await('SELECT mods FROM player_vehicles WHERE plate = ?', {plate})
     if result[1] ~= nil then
@@ -49,7 +49,7 @@ QBCore.Functions.CreateCallback("qb-mygaragemenu:server:GetVehicleProperties", f
     cb(properties)
 end)
 
-QBCore.Functions.CreateCallback("qb-mygaragemenu:server:getMyVehicles", function(source, cb)
+QBCore.Functions.CreateCallback("mh-mygaragemenu:server:getMyVehicles", function(source, cb)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local citizenid = Player.PlayerData.citizenid
