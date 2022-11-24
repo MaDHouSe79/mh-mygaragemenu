d8c3e039a199f373bc7245b2ba0d62dd2397a210
MaDHouSe79 <madhouse1979@gmail.com>
2022-11-24 14:30:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,18 @@
-Config                  = {}
+Config = {}
 
-Config.CheckForUpdates  = true
-
-Config.KeyScriptTrigger = "qb-vehiclekeys:server:AcquireVehicleKeys"
-
-Config.ParkingCommand   = "park-menu"
-Config.ParkingKeybinds  = "F6"
-
-Config.AutoStartVehicle = true
-
-Config.AdminOnly        = true
-
-Config.fontawesome = {
-    close_menu = "fa-solid fa-angle-left",
-    item_menu  = "fa-solid fa-angle-right",
-    open_menu  = "fa-solid fa-circle-info",
-}
-
-Config.Authorized = {
-    "CYC60989",
+Config.Categories = {
+    ['cycles']      = 'Cycles',
+    ['motorcycles'] = 'Motorcycles',
+    ['compacts']    = 'Compacts',
+    ['coupes']      = 'Coupes',
+    ['sedans']      = 'Sedans',
+    ['offroad']     = 'Offroad',
+    ['muscle']      = 'Muscle',
+    ['suvs']        = 'Suvs',
+    ['speed']       = 'Speed',
+    ['sports']      = 'Sport',
+    ['super']       = 'Super',
+    ['vans']        = 'Vans',
+    ['planes']      = 'Planes',
+    ['helicopters'] = 'Helicopters',
 }
